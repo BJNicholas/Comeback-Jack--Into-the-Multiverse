2e1afa744fbfd122c106d735522a64989e794503
wrobe71279 <85209846+wrobe71279@users.noreply.github.com>
2022-03-22 10:28:13 +1100

    Created Tutorial
    - Updated Main Menu scene to add tutorial
    - Updated Main Menu script to add tutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] private GameObject mainMenuUI;
+    [SerializeField] private GameObject tutorialUI;
+
+    private void Awake()
+    {
+        mainMenuUI.SetActive(true);
+        tutorialUI.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void Tutorial()
+    {
+        mainMenuUI.SetActive(false);
+        tutorialUI.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
